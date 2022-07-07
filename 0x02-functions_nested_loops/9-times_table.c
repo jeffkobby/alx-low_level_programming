1b9fd2471ac8f9f741aedf2ba855ba517542a754
Jeffrey Asante <jeffkobby@gmail.com>
2022-07-06 22:31:33 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,9 +19,9 @@ void times_table(void)
 			_putchar(' ');
 			prod = num * mult;
 
-			if (prod <= 9 && prod == 0)
+			if (prod <= 9)
 			{
-				_putchar('0');
+				_putchar(prod);
 			}
 			else
 			{
