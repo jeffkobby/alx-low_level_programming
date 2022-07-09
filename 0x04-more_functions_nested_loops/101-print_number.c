e2646b198a9b42d9509943c297d4182b4f0677fc
Jeffrey Asante <jeffkobby@gmail.com>
2022-07-08 21:51:51 -0700

    assignment

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *  * print_number - prints an integer.
+ *   * @n: input integer.
+ *    * Return: no return.
+*/
+
+void print_number(int n)
+{
+	unsigned int s;
+
+	if (n < 0)
+	{
+		s = -n;
+		_putchar('-');
+	}
+
+	else
+	{
+		s = n;
+	}
+
+	if (s / 10)
+	{
+		print_number(s / 10);
+	}
+
+	_putchar((s % 10) + '0');
+
+}
